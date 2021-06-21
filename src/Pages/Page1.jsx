d1115e473094de67b31266dbb4641439ca1f9aaf
Artur Ribeiro <profissional.arturrc@gmail.com>
2021-06-21 00:21:47 -0300

    Criação das Rotas com React Router

---
+++ src/Pages/Page1.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Page1 = () => {
+  return (
+    <div
+      style={{
+        background: "blue",
+        fontSize: "40px",
+        color: "#fff",
+        width: "100%",
+        height: "100vh",
+      }}
+    >
+      <h1>Oi eu sou a página 1</h1>
+    </div>
+  );
+};
+
+export default Page1;
---
+++ src/Pages/Page2.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Page2 = () => {
+  return (
+    <div
+      style={{
+        background: "red",
+        fontSize: "40px",
+        color: "#fff",
+        width: "100%",
+        height: "100vh",
+      }}
+    >
+      <h1>Oi eu sou a página 2</h1>
+    </div>
+  );
+};
+
+export default Page2;
